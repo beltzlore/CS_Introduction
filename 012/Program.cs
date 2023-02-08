e662181c9d5cd78d8d7241a03d7a3631124d05a7
Anastasia Pupyreva <beltzlore@gmail.com>
2023-02-08 11:57:38 +0300

    Lesson1 04

--- 012/Program.cs
+++ 012/Program.cs
@@ -4,7 +4,7 @@ System.Console.Write("Введите число: ");
 int N=Convert.ToInt32(System.Console.ReadLine());
 int d0,d1;
 d0=N%10;
-d1=N/10%10;
+d1=N/10;
 if (d0>d1) 
 {
     System.Console.WriteLine($"Наибольшая цифра числа: {d0}");
