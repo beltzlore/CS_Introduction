3395dbfc0f92be0adf302a1b645b9c8334e2f94f
Anastasia Pupyreva <beltzlore@gmail.com>
2023-03-08 18:25:37 +0300

    Lesson8 01

---
+++ 088/Program.cs
@@ -0,0 +1,8 @@
+﻿// 88. Заменить все вхождения подстроки w в строке st на строку v. 
+// Вывести на экран первоначальную строку и конечную строку.
+
+string st="58712786126867";
+string w="12";
+string v="000";
+System.Console.WriteLine(st);
+System.Console.WriteLine(st.Replace(w,v));
