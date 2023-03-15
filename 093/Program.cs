62fd8ea2e9c1864fe6b5f539f192c0a70eea7a8e
Anastasia Pupyreva <beltzlore@gmail.com>
2023-03-15 19:10:51 +0300

    Lesson9 01

---
+++ 093/Program.cs
@@ -0,0 +1,33 @@
+﻿// 93. Задайте две матрицы. Напишите программу с подпрограммой MultiMatrix,
+// которая  будет находить произведение двух матриц. 
+// а) простым поэлементным перемножением.
+
+int[,] MultiMatrix (int[,] m1, int[,] m2)
+{
+	int[,] m = new int[m1.GetLength(0),m1.GetLength(1)];
+	for (int i=0;i<m.GetLength(0);++i)
+		for (int j=0;j<m.GetLength(1);++j)
+			m[i,j]=m1[i,j]*m2[i,j];
+	return m;
+}
+
+void PrintMatrix (int[,] m)
+{
+	for (int i=0;i<m.GetLength(0);++i)
+	{
+		for (int j=0;j<m.GetLength(1);++j)
+			System.Console.Write($"{m[i,j],4}");
+		System.Console.WriteLine();
+	}
+}
+
+int[,] matrix1 = { {1, 4, 7, 2},
+                             {5, 9, 2, 3},
+                             {8, 4, 2, 4},
+                             {5, 2, 6, 7}};
+int[,] matrix2 = { {1, 5, 8, 5},
+                             {4, 9, 4, 2},
+                             {7, 2, 2, 6},
+                             {2, 3, 4, 7}};
+int[,] matrix3 = MultiMatrix (matrix1, matrix2);
+PrintMatrix (matrix3);
---
+++ 098/Program.cs
@@ -0,0 +1,26 @@
+﻿// 98. Показать треугольник Паскаля.
+
+int PascalsTriangle (int i, int j)
+{
+	int t=0;
+	if (i==1 && j==0) t=1;
+	if (j>0 && i>0) t=PascalsTriangle(i-1,j-1)+ PascalsTriangle(i,j-1);
+	return t;
+}
+
+void PrintTriangle (int[,] m)
+{
+	for (int j=0;j<m.GetLength(1);++j)
+	{
+		for (int i=0;i<=j+1;++i)
+			System.Console.Write($"{m[i,j],4}");
+		System.Console.WriteLine();
+	}
+}
+
+int size=10;
+int[,] triangle = new int [size+1,size];
+for (int i=0;i<size+1;++i)
+	for (int j=0;j<size;++j)	
+		triangle[i,j]= PascalsTriangle(i,j);
+PrintTriangle(triangle);
